176464b58730c9e0bf56fd301ae4465b22b5cf6b
ycswaves <ycswaves@gmail.com>
2014-09-23 08:54:34 +0200

    make upload image public

--- collections/images_col.js
+++ collections/images_col.js
@@ -1,7 +1,7 @@
 var imageStore = new FS.Store.S3("images", {
   region: "ap-southeast-1",
   bucket: "yamato-image", //required
-  // ACL: myValue, //optional, default is 'private', but you can allow public or secure access routed through your app URL
+  ACL: "public", //optional, default is 'private', but you can allow public or secure access routed through your app URL
   // // The rest are generic store options supported by all storage adapters
   // transformWrite: myTransformWriteFunction, //optional
   // transformRead: myTransformReadFunction, //optional
