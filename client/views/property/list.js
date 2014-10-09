d7c413721535c7b953455a95701f1cc6df9fe60c
ycswaves <ycswaves@gmail.com>
2014-10-09 12:15:25 +0200

    pagination phase1

--- client/views/property/list.js
+++ client/views/property/list.js
@@ -20,6 +20,8 @@ ListController = RouteController.extend({
     var params = this.params
       , pageLimit = 8
       , pageNum = 1
+      , windowSize = 5 // must be odd number
+      , midIndex = parseInt(windowSize / 2)
       , filter = {}
       , result = {};
     // if(params.id){ //TODO: testing get query from URL
@@ -29,8 +31,24 @@ ListController = RouteController.extend({
       pageNum = params.page;
     }
     console.log('pageNum: '+pageNum, 'query: '+params.query);
+
+    var dbQuery = Properties.find({}, {sort: {createdAt: -1} }) //filter apply here too
+      , totalDocs = dbQuery.count()
+      , paginatedDocs = Properties.find({}, {sort: {createdAt: -1}, skip: (pageNum-1)*pageLimit, limit: pageLimit})
+      , startPage = (pageNum <= midIndex)? 1 : (pageNum - midIndex);
+
+    var pageNumberArr = Array.apply(null, Array(windowSize))
+                      .map(function (e, i) {
+                        var res = i+startPage;
+                        if(res > totalDocs) return false; // don't exceed page limit
+                        return res;
+                      });
+
     return {
-      properties: Properties.find({}, {sort: {createdAt: -1}, skip: (pageNum-1)*pageLimit, limit: pageLimit})
+      properties: paginatedDocs,
+      totalDocs: totalDocs,
+      paginations: pageNumberArr,
+      currentPage: pageNum
     }
   }
 });
