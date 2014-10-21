91c6cd9b190b77ccd6ab2619750fe6de11e55100
ycswaves <ycswaves@gmail.com>
2014-10-21 12:33:39 +0200

    Updates on Conversation module

--- client/lib/Message/js/conversations.js
+++ client/lib/Message/js/conversations.js
@@ -1,73 +1,102 @@
 conversations = new Meteor.Collection(null);
 
 Conversations = {
-  init: function(topicId) {
-    if(conversations.find({topicId:topicId}).count() == 0){
-      conversations.insert({topicId:topicId});
-    }
-  },
-  start: function(referId, type) {
-    //means there is no same topic
-    if(Topics.find({creator:Meteor.userId(),referId:referId}).count() == 0) {
-      var formObj = {
-        creator: Meteor.userId(),
-        referId: referId
-      };
-      Meteor.call('addTopic', formObj, function(err, topicId){
-        if(err){
-          console.log('Add Topic: '+err);
-          return false;
-        }
-        Conversations.init(topicId);
-      });
-    }
-    else {
-      var topic = Topics.findOne({creator:Meteor.userId(),referId:referId});
-      Conversations.init(topic._id);
-    }
-  },
-  send: function(topicId, content) {
-    Meteor.call('addConversation',topicId,content,function(err){
-      if(err){
-        console.log('Send PM: '+err);
-        return false;
-      }
-    });
-  }
+	init: function(topicId) {
+		if(conversations.find({topicId:topicId}).count() == 0){
+			conversations.insert({topicId:topicId});
+		}
+	},
+	start: function(referId, type) {
+		//means there is no same topic
+		if(Topics.find({creator:Meteor.userId(),referId:referId}).count() == 0) {
+			var formObj = {
+				creator: Meteor.userId(),
+				referId: referId
+			};
+			Meteor.call('addTopic', formObj, function(err, topicId){
+				if(err){
+					console.log('Add Topic: '+err);
+					return false;
+				}
+				Conversations.init(topicId);
+			});
+		}
+		else {
+			var topic = Topics.findOne({creator:Meteor.userId(),referId:referId});
+			Conversations.init(topic._id);
+		}
+	},
+	sendAsync: function(topicId, content, callback) {
+		Meteor.call('addConversation',topicId,content,function(err, res){
+			if(err){
+				console.log('Send PM: '+err);
+				callback(err. false);
+			}
+			callback(null, res);
+		});
+	},
+	retrieve: function(topicId) {
+		var messages = Messages.find({topicId:topicId,owner:Meteor.userId()}).fetch();
+
+		Meteor.call('retrieveConversation',topicId,function(err,data){
+			if(err){
+				console.log('Send PM: '+err);
+				callback(err, []);
+			}
+			callback(null, data);
+		});
+	}
 }
 
 Template.conversationTopics.helpers({
-   conversationTopics : function () {
-    var conversationTopics = conversations.find().fetch();
-    $.each(conversationTopics, function(index, value) {
-      value.group = value.message instanceof Array;
-    });
-    return conversationTopics;
-  }
+	conversationTopics : function () {
+		var conversationTopics = conversations.find().fetch();
+		return conversationTopics;
+	}
+});
+
+Template.conversationTopic.helpers({
+	messages : function (topicId) {
+		var messages = Messages.find({topicId:topicId,owner:Meteor.userId()}).fetch();
+		return messages;
+	}
 });
 
 Template.conversationTopic.rendered = function () {
-  $('.headWrapper').popover({
-    html : true, 
-    content: function() {
-      return $('#conversation-box').html();
-    },
-    title: "私信",
-    animation: false,
-    placement: "right",
-    trigger: "click",
-    template: '<div class="popover conversation-popover" role="tooltip"><div class="arrow sidearrow"></div><h3 class="popover-title"></h3><div class="popover-content conversation-content background-color-grey-light"></div></div>'
-  });
+	$('body').on('keypress','.PMInput',function(e) {
+		var topicId = $(this).data('topicId');
+		var content = $(this).val();
+		if(10==e.which && content!="") {	
+			Conversations.sendAsync(topicId,content, function(err, res){
+				if(res){
+					$('.PMInput').val('');
+				}
+			})
+		}
+	})
+	/*
+	$('.headWrapper').popover({
+		html : true, 
+		content: function() {
+			return $('#conversation-box').html();
+		},
+		title: "私信",
+		animation: false,
+		placement: "right",
+		trigger: "click",
+		template: '<div class="popover conversation-popover" role="tooltip"><div class="arrow sidearrow"></div><h3 class="popover-title"></h3><div class="popover-content conversation-content background-color-grey-light"></div></div>'
+	});
 
-  $('.Conversation').on('shown.bs.popover', function () {
-    $('.conversation-popover').css('top',parseInt($('.conversation-popover').css('top')) - 156 + 'px')
-  });
+	$('.Conversation').on('shown.bs.popover', function () {
+		$('.conversation-popover').css('top',parseInt($('.conversation-popover').css('top')) - 156 + 'px')
+	});
+	*/
 
-  $('body').on('mouseenter','.Conversation',function(){
-    $('.Conversation').find('.cancelButton').css('visibility','visible');
-  });
+	$('body').on('mouseenter','.Conversation',function(){
+		$('.Conversation').find('.cancelButton').css('visibility','visible');
+	});
 
-  $('body').on('mouseleave','.Conversation',function(){
-    $('.Conversation').find('.cancelButton').css('visibility','hidden');
-  });
+	$('body').on('mouseleave','.Conversation',function(){
+		$('.Conversation').find('.cancelButton').css('visibility','hidden');
+	});
 }
--- client/router.js
+++ client/router.js
@@ -1,14 +1,14 @@
 Router.configure({
-  // notFoundTemplate: 'notFound',
-  // loadingTemplate: 'loading',
-  layoutTemplate: 'layout'
+// notFoundTemplate: 'notFound',
+// loadingTemplate: 'loading',
+layoutTemplate: 'layout'
 });
 
 var filters = {
 	isLoggedIn: function(pause) {
 		if (!(Meteor.user() || Meteor.loggingIn())) {
 			Router.go('landing');
-      pause();
+			pause();
 		}
 	}
 };
@@ -19,89 +19,89 @@ Router.map(function () {
 	this.route('landing', {
 		path: '/',
 		template: 'landingPage',
-    action: function () {
-     this.render();
-    }
+		action: function () {
+			this.render();
+		}
 	});
 
-  this.route('signup', {
-    path: '/account/signup',
-    template: 'signupForm',
-    action: function () {
-     this.render();
-    }
-  });
+	this.route('signup', {
+		path: '/account/signup',
+		template: 'signupForm',
+		action: function () {
+			this.render();
+		}
+	});
 
-  this.route('profile', {
-    path: '/profile',
-    waitOn: function () {
-      return Meteor.subscribe('userData');
-    },
-    template: 'profilePage',
-    action: function () {
-     this.render();
-    }
-  });
+	this.route('profile', {
+		path: '/profile',
+		waitOn: function () {
+			return Meteor.subscribe('userData');
+		},
+		template: 'profilePage',
+		action: function () {
+			this.render();
+		}
+	});
 
-  this.route('myproperty', {
-    path: '/myproperty/list/:page',
-    controller: 'MyPropertiesController'
-  });
+	this.route('myproperty', {
+		path: '/myproperty/list/:page',
+		controller: 'MyPropertiesController'
+	});
 
-  this.route('properties', {
-    path: '/properties/list/:page',
-    controller: 'ListController'
-  });
+	this.route('properties', {
+		path: '/properties/list/:page',
+		controller: 'ListController'
+	});
 
-  this.route('addProperty', {
-    path: '/properties/add',
-    controller: 'AddPropertyController'
-  });
+	this.route('addProperty', {
+		path: '/properties/add',
+		controller: 'AddPropertyController'
+	});
 
-  this.route('editProperty', {
-    path: '/properties/edit/:id',
-    controller: 'EditPropertyController'
-  });
+	this.route('editProperty', {
+		path: '/properties/edit/:id',
+		controller: 'EditPropertyController'
+	});
 
-  this.route('propertyDetail', {
-    path: '/property/:id',
-    waitOn: function () {
-      return Meteor.subscribe('propertyDetail', this.params.id);
-    },
-    template: 'propertyDetail',
-    action: function () {
-      if (this.ready()){
-        this.render();
-      }
-      else{
-        this.render('loading');
-      }
-    },
-    data: function () {
-      var params = this.params;
-      var property = Properties.findOne({_id: params.id});
-      if(property.author != Meteor.userId()) {
-        var isNotOwner = true;
-      }
-      else {
-        var isNotOwner = false;
-      }
-      return {
-        property: property,
-        isNotOwner: isNotOwner
-      }
-    }
-  });
+	this.route('propertyDetail', {
+		path: '/property/:id',
+		waitOn: function () {
+			return Meteor.subscribe('propertyDetail', this.params.id);
+		},
+		template: 'propertyDetail',
+		action: function () {
+			if (this.ready()){
+				this.render();
+			}
+			else{
+				this.render('loading');
+			}
+		},
+		data: function () {
+			var params = this.params;
+			var property = Properties.findOne({_id: params.id});
+			var isNotOwner = false;
+			if(typeof property!="undefined"){
+				if(property.author != Meteor.userId()) {
+					var isNotOwner = true;
+				}
+			}
+			return {
+				property: property,
+				isNotOwner: isNotOwner
+			}
+		}
+	});
 
-  this.route('inbox', {
-    path: '/inbox',
-    template: 'inboxPage',
-    action: function () {
-      this.render();
-    }
-  });
+	this.route('inbox', {
+		path: '/inbox',
+		template: 'inboxPage',
+		action: function () {
+			this.render();
+		}
+	});
 
-  // matches all urls but doesn't get called until all previous routes have been tested
-  // so in this case for invalid url
-  this.route('notFound', {path: '*'});
+	// matches all urls but doesn't get called until all previous routes have been tested
+	// so in this case for invalid url
+	this.route('notFound', {path: '*'});
 })
