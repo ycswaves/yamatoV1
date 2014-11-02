dbfabdfa8a91e7abdf08d5e3d0b3fa2be29ed879
GUANHAO SHEN <daven.shen@gmail.com>
2014-11-03 00:25:55 +0800

    Update
    增加momentjs
    改变inbox style

--- client/views/inbox/inbox.js
+++ client/views/inbox/inbox.js
@@ -4,8 +4,33 @@ Template.inboxPage.rendered = function() {
 
 Template.inboxPage.helpers({
   messages: function(){
-    // same as publication
-    return Messages.find({}, {sort: {updatedAt: -1}, limit: 5});
-  }
-
+		var unreads = Messages.find(
+			{
+				owner: Meteor.userId(),
+				isValid: true
+			}
+		);
+		if (unreads.count() > 0) {
+			var returnTopics = [];
+			var groupedTopics = _.groupBy(_.pluck(unreads.fetch(), 'topicId'));
+			_.each(_.values(groupedTopics), function(topics) {
+				var topic = Topics.findOne({_id:topics[0]});
+				if(topic){
+					var lastMessage = Messages.findOne({topicId:topic._id,owner:Meteor.userId(),receiver:Meteor.userId()},{sort: {createdAt : -1}});
+					if(lastMessage){
+						var sender = Meteor.users.findOne({_id:lastMessage.sender});
+						var username = sender.username;
+						topic.sender = username;
+						topic.message = lastMessage.content;
+						topic.lastTime = moment(lastMessage.createdAt).fromNow();
+						returnTopics.push(topic);
+					}
+				}
+			});
+			return returnTopics;
+		}
+		else {
+			return false;
+		};
+	}
 });
