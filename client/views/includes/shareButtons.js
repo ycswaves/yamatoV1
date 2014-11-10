8bdb7016d733db797a3363f2b0f0bb9316cc6507
GUANHAO SHEN <daven.shen@gmail.com>
2014-11-10 23:42:22 +0800

    Monior
    Change text of share buttons

--- client/views/includes/shareButtons.js
+++ client/views/includes/shareButtons.js
@@ -12,11 +12,11 @@ Template.shareButtons.rendered = function(){
 	})
 	//分享到人人
 	$('body').off('click','#renrenBtn').on('click','#renrenBtn',function(){
-		CommonHelper.popupWindow('http://widget.renren.com/dialog/share?resourceUrl='+Router.current().originalUrl, '分享至新浪微博', 200, 200);
+		CommonHelper.popupWindow('http://widget.renren.com/dialog/share?resourceUrl='+Router.current().originalUrl, '分享至人人', 200, 200);
 	})
 	//分享到Facebook
 	$('body').off('click','#facebookBtn').on('click','#facebookBtn',function(){
-		CommonHelper.popupWindow('https://www.facebook.com/sharer/sharer.php?u='+Router.current().originalUrl, '分享至新浪微博', 600, 300);
+		CommonHelper.popupWindow('https://www.facebook.com/sharer/sharer.php?u='+Router.current().originalUrl, '分享至Facebook', 600, 300);
 	})
 }
 
