9f01a925cd1711fedd296f64eb0e69c54b8ed2e7
Guanhao <daven.shen@gmail.com>
2014-11-17 10:01:28 +0800

    Fix indentation

--- client/views/includes/shareButtons.js
+++ client/views/includes/shareButtons.js
@@ -1,24 +1,24 @@
 Template.wechatPopup.rendered = function(){
-	//分享到微信
-	var shareUri = document.URL;
-	$('#wechatModalContent').qrcode({
-		text: shareUri
-	});
+  //分享到微信
+  var shareUri = document.URL;
+  $('#wechatModalContent').qrcode({
+    text: shareUri
+  });
 }
 
 Template.shareButtons.rendered = function(){
-	var shareUri = document.URL;
-	//分享到微博
-	$('body').off('click','#weiboBtn').on('click','#weiboBtn',function(){
-		CommonHelper.popupWindow('http://service.weibo.com/share/share.php?url='+shareUri, '分享至新浪微博', 200, 200);
-	})
-	//分享到人人
-	$('body').off('click','#renrenBtn').on('click','#renrenBtn',function(){
-		CommonHelper.popupWindow('http://widget.renren.com/dialog/share?resourceUrl='+shareUri, '分享至人人', 200, 200);
-	})
-	//分享到Facebook
-	$('body').off('click','#facebookBtn').on('click','#facebookBtn',function(){
-		CommonHelper.popupWindow('https://www.facebook.com/sharer/sharer.php?u='+shareUri, '分享至Facebook', 600, 300);
-	})
+  var shareUri = document.URL;
+  //分享到微博
+  $('body').off('click','#weiboBtn').on('click','#weiboBtn',function(){
+    CommonHelper.popupWindow('http://service.weibo.com/share/share.php?url='+shareUri, '分享至新浪微博', 200, 200);
+  })
+  //分享到人人
+  $('body').off('click','#renrenBtn').on('click','#renrenBtn',function(){
+    CommonHelper.popupWindow('http://widget.renren.com/dialog/share?resourceUrl='+shareUri, '分享至人人', 200, 200);
+  })
+  //分享到Facebook
+  $('body').off('click','#facebookBtn').on('click','#facebookBtn',function(){
+    CommonHelper.popupWindow('https://www.facebook.com/sharer/sharer.php?u='+shareUri, '分享至Facebook', 600, 300);
+  })
 }
 
