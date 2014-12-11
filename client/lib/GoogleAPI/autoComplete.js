831010328f17d139fdd25d3831a5e36238ba1579
YiChenshu <ycswaves@gmail.com>
2014-12-11 22:09:28 +0800

    create GoogleAPI folder

---
+++ client/lib/GoogleAPI/autoComplete.js
@@ -0,0 +1,14 @@
+GoogleAutoComplete = {
+  'init': function(){
+    // Create the autocomplete object, restricting the search
+    // to geographical location types.
+    autocomplete = new google.maps.places.Autocomplete(
+        /** @type {HTMLInputElement} */(document.getElementById('autocomplete')),
+        { types: ['geocode'] });
+    // When the user selects an address from the dropdown,
+    // populate the address fields in the form.
+    google.maps.event.addListener(autocomplete, 'place_changed', function() {
+      fillInAddress();
+    });
+  }
+}
