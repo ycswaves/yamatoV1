289738fbe549ba6d53c6d5c129295251c62ffc42
YiChenshu <ycswaves@gmail.com>
2014-12-15 23:31:59 +0800

    update googlemap call

--- server/lib/httpRequest.js
+++ server/lib/httpRequest.js
@@ -1,5 +1,9 @@
 Meteor.methods({
   'get' : function(url,options){
     return HTTP.get(url,options);
+  },
+  'googleMapAPI' : function(url,options){
+    options.params.key = process.env.GOOGLE_MAP_API_KEY;
+    return HTTP.get(url,options);
   }
 });
