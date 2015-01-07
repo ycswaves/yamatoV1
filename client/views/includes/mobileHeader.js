d3b44585238d1eae5f770aeb6e6dd9fe0595197f
Guanhao <daven.shen@gmail.com>
2015-01-07 16:01:34 +0800

    update mobile nav and listing search

---
+++ client/views/includes/mobileHeader.js
@@ -0,0 +1,38 @@
+Template.mobileHeader.rendered = function(){
+
+}
+
+Template.mobileHeader.events({
+  //sign out
+  'click #signout' : function(e, t){
+    e.preventDefault();
+    Meteor.logout(function(){
+      conversations.remove({});
+      if (typeof Session.get('currentPath') != 'undefined') {
+        Router.go(Session.get('currentPath')); //返回当前页面
+      }
+      else {
+        Router.go('landing');
+      }
+      delete Session.keys['currentPath']; //删除当前Session
+      delete Session.keys['resetPassword'];
+    });
+  }
+})
+
+Template.mobileHeader.helpers({
+  loggedInUser: function(){
+    return Meteor.user();
+  },
+  messageCount: function(){
+    var count = Messages.find(
+      {
+        owner: Meteor.userId(),
+        receiver: Meteor.userId(),
+        isRead: false,
+        isValid: true
+      }
+    ).count();
+    return count || false;
+  }
+})
