f2e8fd5d4406952f68c62af5d78ea525c4b5d63d
YiChenshu <ycswaves@gmail.com>
2015-01-18 23:14:23 +0800

    prevent multi call in multi address

--- client/lib/GoogleAPI/googleDirection.js
+++ client/lib/GoogleAPI/googleDirection.js
@@ -1,12 +1,13 @@
 /**
- * directions 
+ * Directions
  * [{ referId: String,
- *   address: String,
- *   distance: String,
- *   duration: String,
+ *   to: lat, lng,
+ *   from: lat, lng,
+ *   error: Boolean,
+ *   response: Obj
  *   display: Boolean }]
  */
-directions = new Mongo.Collection(null);
+Directions = new Mongo.Collection(null);
 
 var directionURL = "https://maps.googleapis.com/maps/api/directions/json";
 
@@ -30,57 +31,57 @@ GoogleDirection = {
     if (mode == "transit") {
       base_params.departure_time = Math.floor(new Date().getTime()/1000); //一定需要
     }
-    Meteor.call('get',directionURL,{
-      params:base_params
-    },function(error,response){
-      //if an error happened, error argument contains the details
-      //if the request succeeded, the response will contain the response of the server request
-      if (error) {
-        callback(null);
-      }
-      else {
-        if (response.data.status == "OK") {
-          if (mode == "transit") {
-            $.each(response.data.routes, function(i, route){
-              var routing = [];
-              var steps = route.legs[0].steps;
-              $.each(steps,function(o, step){
-                var hasIcon = true;
-                var bgColor = null;
-                var shortName = null;
-                if (step.travel_mode=="TRANSIT") {
-                  if (typeof step.transit_details.line.color != "undefined") {
-                    hasIcon = false;
-                    bgColor = step.transit_details.line.color;
-                  }
-                  shortName = step.transit_details.line.short_name;
-                }
-                routing.push({
-                  mode:step.travel_mode.toLowerCase(),
-                  hasIcon:hasIcon, //需不需要显示图标
-                  bgColor:bgColor,
-                  shortName:shortName
-                });
-              })
-              response.data.routes[i].routing = routing;
-            })
-          }
-          else {
-            $.each(response.data.routes, function(i, route){
-              response.data.routes[i].routing = [{mode:mode,hasIcon:true}];
-            })
-          }
-          callback(response.data);
-        }
-        else {
+    Meteor.call('get',directionURL,{params:base_params},
+      function(error,response){
+        //if an error happened, error argument contains the details
+        //if the request succeeded, the response will contain the response of the server request
+        if (error) {
           callback(null);
         }
+        else {
+          if (response.data.status == "OK") {
+            if (mode == "transit") {
+              $.each(response.data.routes, function(i, route){
+                var routing = [];
+                var steps = route.legs[0].steps;
+                $.each(steps,function(o, step){
+                  var hasIcon = true;
+                  var bgColor = null;
+                  var shortName = null;
+                  if (step.travel_mode=="TRANSIT") {
+                    if (typeof step.transit_details.line.color != "undefined") {
+                      hasIcon = false;
+                      bgColor = step.transit_details.line.color;
+                    }
+                    shortName = step.transit_details.line.short_name;
+                  }
+                  routing.push({
+                    mode:step.travel_mode.toLowerCase(),
+                    hasIcon:hasIcon, //需不需要显示图标
+                    bgColor:bgColor,
+                    shortName:shortName
+                  });
+                })
+                response.data.routes[i].routing = routing;
+              })
+            }
+            else {
+              $.each(response.data.routes, function(i, route){
+                response.data.routes[i].routing = [{mode:mode,hasIcon:true}];
+              })
+            }
+            callback(response.data);
+          }
+          else {
+            callback(null);
+          }
+        }
       }
-    })
+    );
   },
 
   //from, to 均为坐标
-  shortest : function(from,to,mode,callback) {
+  shortest : function(from,to,mode) {
     var base_params = {
           origin : from,
           destination : to,
@@ -91,26 +92,36 @@ GoogleDirection = {
     if (mode == "transit") {
       base_params.departure_time = Math.floor(new Date().getTime()/1000);//一定需要
     }
-    Meteor.call('get',directionURL,{
-      params:base_params
-    },function(error,response){
-      if (error) {
-        callback(error);
-      }
-      else {
-        if (response.data.status == "OK") {
-          var distance = null;
-          var duration = null;
-          $.each(response.data.routes, function(i, route){
-            distance = route.legs[0].distance.value;
-            duration = route.legs[0].duration.value;
-          })
-          callback(null, {distance:distance,duration:duration});
+
+    var existed = Directions.findOne({from:from, to:to});
+    if(existed && existed.response){
+      //console.log(from, to, 'no call');
+    } else {
+      var defaultRecord = {
+        from:from,
+        to:to,
+        error: 'waiting for response',
+        response: null,
+        display: true
+      };
+      Directions.insert(defaultRecord); //make a record first to prevent next repeated async call
+      Meteor.call('get',directionURL,{params:base_params},
+        function(error,response){
+          if (error) {
+            Directions.update({from: from, to: to}, {$set:{error: error}});
+          }
+          else {
+            if (response.data.status == "OK") {
+              var waiting = Directions.findOne(defaultRecord);
+              if(waiting){
+                Directions.insert({from: from, to: to, error: null, response: response, display:true});
+                Directions.remove(defaultRecord); //meteor mongo does not support unique index yet,
+                                                  //so here need to manually delete the default msg
+              }
+            }
+          }
         }
-        else {
-          callback(response);
-        }
-      }
-    })
+      ); //end of meteor call
+    }
   }
 }
--- client/views/includes/elements/distanceTo.js
+++ client/views/includes/elements/distanceTo.js
@@ -1,49 +1,21 @@
 Template.distanceTo.helpers({
   getDistances: function(thisProperty){
-    //使用ID做KEY来保存async返回的数据
-    var _id = thisProperty._id;
     var multiAddress = Session.get('multiAddress')
-    , determiner = [];
+    , distances = [];
+
     if(multiAddress){
       for(var key in multiAddress){
-        var geo = multiAddress[key].geometry;
-        var address = multiAddress[key].address;
-        var called = false;
+        //var geo = multiAddress[key].geometry;
+        //var address = multiAddress[key].address;
 
-        //避免重复调用相同地址
-        if(directions.findOne({referId:_id,to:address})){
-          called = true;
-          directions.update({referId: _id,to:address}, {$set:{display: true}});
-        }
+        var fromAddr = multiAddress[key].address //geo.latitude+','+geo.longitude
+          , toAddr = thisProperty.address; //.map.latitude+','+thisProperty.map.longitude;
 
-        if (!called){
-          GoogleDirection.shortest(
-            thisProperty.map.latitude+','+thisProperty.map.longitude, //to
-            geo.latitude+','+geo.longitude, //from, can be addr or lat,lng
-            'driving', //mode
-            function(err, data){ //callback
-              if(err){ // can be null when no result
-                console.log(err);
-              } else {
-                var obj = {
-                  referId: _id,
-                  from: thisProperty.address,
-                  to: address,
-                  distance: data.distance/1000,
-                  duration: Math.ceil(data.duration/60),
-                  display:true
-                };
-                console.log('Call made:'+address);
-                //存入local collection
-                if(directions.find(obj).count()==0){
-                  directions.insert(obj);
-                }
-              }
-            });
-        }
+        GoogleDirection.shortest(fromAddr, toAddr, 'driving');
       }
     }
-    return directions.find({referId:_id,display:true});
+
+    return Directions.find({to:thisProperty.address});
   },
   hasMultiAddr: function(){
     var multiAddress = Session.get('multiAddress');
