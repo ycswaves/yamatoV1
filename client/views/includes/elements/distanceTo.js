61f656a6af3324ab569451879ce01cb69a03f3e2
GUANHAO SHEN <daven.shen@gmail.com>
2015-01-19 22:53:58 +0800

    更新

--- client/views/includes/elements/distanceTo.js
+++ client/views/includes/elements/distanceTo.js
@@ -5,17 +5,29 @@ Template.distanceTo.helpers({
 
     if(multiAddress){
       for(var key in multiAddress){
-        //var geo = multiAddress[key].geometry;
-        //var address = multiAddress[key].address;
-
         var fromAddr = multiAddress[key].address //geo.latitude+','+geo.longitude
-          , toAddr = thisProperty.address; //.map.latitude+','+thisProperty.map.longitude;
+        , toAddr = thisProperty.address; //.map.latitude+','+thisProperty.map.longitude;
 
         GoogleDirection.shortest(fromAddr, toAddr, 'driving');
       }
     }
-
     return Directions.find({to:thisProperty.address});
+  },
+  duration: function(response){
+    if (response) {
+      var leg = response.data.routes[0].legs[0];
+      return leg.duration.text;
+    } else {
+      return null;
+    }
+  },
+  distance: function(response){
+    if (response) {
+      var leg = response.data.routes[0].legs[0];
+      return leg.distance.text;
+    } else {
+      return null;
+    }
   },
   hasMultiAddr: function(){
     var multiAddress = Session.get('multiAddress');
