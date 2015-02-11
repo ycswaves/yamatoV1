2fa6a7ba3379294ce51db3536f5c88cba09ded3d
YiChenshu <ycswaves@gmail.com>
2015-02-11 23:33:55 +0800

    able to pass in crop info

--- client/views/account/change-avartar/my-avartar.js
+++ client/views/account/change-avartar/my-avartar.js
@@ -1,4 +1,4 @@
-var cropped;
+var cropped, file;
 
 Template.uploadAvatar.rendered = function() {
   render();
@@ -7,7 +7,7 @@ Template.uploadAvatar.rendered = function() {
 Template.uploadAvatar.events({
   'change #avatarImg': function(e, t){
     e.preventDefault();
-    var file = t.find('#avatarImg').files[0];
+    file = t.find('#avatarImg').files[0];
     if(file){
       var reader = new FileReader();
       reader.onload = function (e) {
@@ -34,5 +34,17 @@ Template.uploadAvatar.events({
 
   'click #confirm': function(){
     console.log(cropped);
+    var FSObj = new FS.File(file);
+    FSObj['cropInfo'] = cropped;
+    //FSObj.attachData(JSON.stringify(cropped));
+    avatarImages.insert(FSObj, function(err, imageUploaded){
+      if(err){
+        console.log(err);
+        //bcoz it's async, even if upload fail, the rest fields should be saved
+        // and redirect to the property detail
+      } else {
+        console.log(imageUploaded._id);
+      }
+    });
   }
 })
